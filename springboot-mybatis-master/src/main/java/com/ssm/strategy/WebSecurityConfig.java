80db2a234f6aef0f3468daacde6361805df36512
Cchua <vipcchua@hotmail.com>
2016-11-02 09:17:35 +0800

    安全权限更新
    现可以读取数据库的用户表，但是好像不用
    auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema();会出现未知错误
    比如新增的用户却没有权限访问

--- springboot-mybatis-master/src/main/java/com/ssm/strategy/WebSecurityConfig.java
+++ springboot-mybatis-master/src/main/java/com/ssm/strategy/WebSecurityConfig.java
@@ -1,23 +1,33 @@
 package com.ssm.strategy;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.ssm.mapper.TableUserMapper;
+import com.ssm.model.TableUser;
+import com.ssm.model.User;
+
 
 @Configuration
 @EnableWebSecurity
+
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	/*@Autowired
-	DataSource dataSource;*/
-	
+	@Autowired
+	DataSource dataSource;
+	@Autowired
+	private TableUserMapper tableUserMapper;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -35,17 +45,62 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("123").password("123").roles("USER");
+
+
+            	
+              /*  auth
+                .inMemoryAuthentication()
+            	 .withUser("123").password("123").roles("USER");*/
+         
+    	
+    	InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> auths = 
+    			
+        		auth.inMemoryAuthentication();
+    	
+    	
+    	
+          
+    /*	JdbcUserDetailsManagerConfigurer<AuthenticationManagerBuilder> auths = 
+    			
+    		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema();*/
+      
    
-        /*auth
-		.jdbcAuthentication()
-			.dataSource(dataSource)
-			.withDefaultSchema()
-			.withUser("user").password("password").roles("USER").and()
-			.withUser("admin").password("password").roles("USER", "ADMIN");*/
+    	UserDetailsManagerConfigurer<AuthenticationManagerBuilder, 
+    	InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder>>.UserDetailsBuilder authsadd 
+    	= auths.withUser("user").password("123").roles("USER");      
+    	List<TableUser> users = tableUserMapper.Loginuser();
+    	/*authsdd.and()*/
+        
+    	
+ 	/*   for(int a= 0;a<user.size();a++){ }*/
+   
+			for(TableUser user : users){
+				
+				String name = user.getUsername();
+				String password =user.getPassword();
+				String pole =user.getRole();
+				
+				/*
+				  auths.withUser("user").password("password").roles("USER").
+				
+			*/
+					
+				authsadd. and()
+					
+					.withUser(name).password(password).roles(pole);
+				
+				
+				  
+			}
+		
+ 		
+      	
+           
     
+        
+        
+        
+        
     }
     
     
